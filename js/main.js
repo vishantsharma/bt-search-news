e4de636fda7f45481f4e7eec51e20cb29910b7f2
vishantsharma <vishantsharma213@gmail.com>
2021-05-15 13:13:28 +0100

    Initialise the app and render

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="app">
+        <main>
+          <h1> BT React Code Test - by Vishant Sharma - 13/05/21 </h1>
+        </main>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById("root"));
