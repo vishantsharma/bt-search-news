6f90c4c35d843b9e2d1905b41f0715a25945c70c
vishantsharma <vishantsharma213@gmail.com>
2021-05-15 17:55:35 +0100

    Include Header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+function Header() {
+    return (
+      <header className="header">
+        <div className="main-container">
+          <img src="bt-icon.svg" alt="BT Logo" className="bt-logo" />
+          <h1>BT News</h1>
+        </div>
+      </header>
+    );
+  }
+
 function App() {
     return (
       <div className="app">
+       <Header />
         <main>
-          <h1> BT React Code Test - by Vishant Sharma - 13/05/21 </h1>
+          <h1> BT React Code Test - by Vishant Sharma - 15/05/21 </h1>
         </main>
       </div>
     );
